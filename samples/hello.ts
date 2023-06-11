117ea83ec10c9d4c2a96014ebc4d28be91ecee39
anntnzrb <anntnzrb@protonmail.com>
2023-06-11 10:46:28 -0700

    feat: add samples

---
+++ samples/hello.ts
@@ -0,0 +1,11 @@
+let message: string = "Hello, World!";
+let count: number = 10;
+
+function greet(name: string): string {
+    return `Hello, ${name}!`;
+}
+
+let userName: string = "John Doe";
+console.log(message);
+console.log(greet(userName));
+console.log(`The count is: ${count}`);

2be586cb9f7cf12af0019f54e408f155c5b6a8de
anntnzrb <anntnzrb@protonmail.com>
2023-06-10 18:49:33 -0700

    chore: add categories section for each member

--- tsparxer/main.py
+++ tsparxer/main.py
@@ -1,51 +1,56 @@
 import ply.lex as lex
 
-# List of token names.   This is always required
+# ------------------------------------------------------------------------------
+# Tokens
+# ------------------------------------------------------------------------------
 tokens = (
-   'NUMBER',
-   'PLUS',
-   'MINUS',
-   'TIMES',
-   'DIVIDE',
-   'LPAREN',
-   'RPAREN',
+    # JA
+    "ANY",
+    # Paul
+    "RETURN",
+    # Chris
+    "PLUS",
 )
 
-# Regular expression rules for simple tokens
-t_PLUS    = r'\+'
-t_MINUS   = r'-'
-t_TIMES   = r'\*'
-t_DIVIDE  = r'/'
-t_LPAREN  = r'\('
-t_RPAREN  = r'\)'
+# ------------------------------------------------------------------------------
+# Token-RegEx
+# ------------------------------------------------------------------------------
+t_PLUS = r"\+"
+
 
 # A regular expression rule with some action code
 def t_NUMBER(t):
-    r'\d+'
+    r"\d+"
     t.value = int(t.value)
     return t
 
+
 # Define a rule so we can track line numbers
 def t_newline(t):
-    r'\n+'
+    r"\n+"
     t.lexer.lineno += len(t.value)
 
+
 # A string containing ignored characters (spaces and tabs)
-t_ignore  = ' \t'
+t_ignore = " \t"
+
 
 # Error handling rule
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
 
+
+# ------------------------------------------------------------------------------
+
 # Build the lexer
 lexer = lex.lex()
 
 # Test it out
-data = '''
+data = """
 3 + 4 * 10
   + -20 *2
-'''
+"""
 
 # Give the lexer some input
 lexer.input(data)
@@ -54,5 +59,5 @@ lexer.input(data)
 while True:
     tok = lexer.token()
     if not tok:
-        break      # No more input
+        break  # No more input
     print(tok)

807bbcad09bb1b78981370dc51177a38aa838521
anntnzrb <anntnzrb@protonmail.com>
2023-06-14 14:45:19 -0500

    fix: fmt & linting

--- tsparxer/util/lex_out_parser.py
+++ tsparxer/util/lex_out_parser.py
@@ -5,15 +5,17 @@ This module allows parsing lex output and write the parsed results to a file.
 import re
 from typing import Any, Dict, List
 
+
 def parse_lex_output(input: str) -> List[Dict[str, Any]]:
     result = []
     pattern = r"LexToken\((\w+),('(.*?)'|(\d+)),(\d+),(\d+)\)"
     for line in input.splitlines():
         match = re.match(pattern, line)
         if match:
-            token_type, value = match.group(1), match.group(3) or match.group(4)
-            result.append({"type": token_type, "value": value})
+            t_type, t_val = match.group(1), match.group(3) or match.group(4)
+            result.append({"type": t_type, "value": t_val})
     return result
+
 
 def parse() -> None:
     # specify input and output files
@@ -21,9 +23,11 @@ def parse() -> None:
     FILE_OUT: str = "data/parsed.out"
 
     # read input & write to file
-    with open(FILE_IN, 'r') as file:
+    with open(FILE_IN, "r") as file:
         lex_output = file.read()
 
-    with open(FILE_OUT, 'w') as FILE_OUT:
+    with open(FILE_OUT, "w") as file:
         for item in parse_lex_output(lex_output):
-            FILE_OUT.write(str(item) + '\n')
+            file.write(str(item) + "\n")
+
+    print("Parsing successful, be wary of strings and numbers mixing.")

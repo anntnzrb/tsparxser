ddde6e4c338a4f27ad736b667a56cc8c239648d0
anntnzrb <anntnzrb@protonmail.com>
2023-06-23 15:02:39 -0500

    test(parser): add run_test for abstraction

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,5 +1,6 @@
 import unittest
 from ply.lex import LexToken
+from typing import List, Tuple
 
 from tsparxer.lexer import Lexer as TSLexer
 from tsparxer.parser import Parser as TSParser
@@ -17,6 +18,20 @@ class TestParser(unittest.TestCase):
         """
         self.lexer: TSLexer = TSLexer()
         self.parser: TSParser = TSParser(self.lexer)
+
+    def run_test(self, tests: List[Tuple[str, bool]]) -> None:
+        """
+        Run a series of tests for the parser.
+
+        :param tests: A list of tuples containing the test data and a boolean
+         indicating if the test is valid or not.
+        """
+        for data, valid in tests:
+            if valid:
+                self.parser.parse(data)
+            else:
+                with self.assertRaises(ParserSyntaxError):
+                    self.parser.parse(data)
 
     # -------------------------------------------------------------------------
     # Tests
@@ -37,12 +52,7 @@ class TestParser(unittest.TestCase):
             ("let", False),
         ]
 
-        for data, valid in tests:
-            if valid:
-                self.parser.parse(data)
-            else:
-                with self.assertRaises(ParserSyntaxError):
-                    self.parser.parse(data)
+        self.run_test(tests)
 
 
 if __name__ == "__main__":

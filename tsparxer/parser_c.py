062142d6d6747a415f09735cc09805d2c43614c8
CristopherVilla20 <cgvilla@espol.edu.ec>
2023-06-26 00:24:09 -0500

    feat: add array, for and function arrow

--- tsparxer/parser_c.py
+++ tsparxer/parser_c.py
@@ -41,6 +41,64 @@ class Parser:
                          | FALSE
         """
 
+    def p_assignment_values(self, p: YaccProduction) -> None:
+        """
+        assignment_values : assignment_value 
+                          | assignment_value COMMA assignment_values
+        """
+    
+    def p_array(self, p: YaccProduction) -> None:
+        """
+        array : OPENBRACKET  assignment_values CLOSEBRACKET
+        """
+
+    def p_assignment_array(self, p: YaccProduction) -> None:
+        """
+        assignment : assignment_type ID COLON data_type OPENBRACKET CLOSEBRACKET EQUALS array SEMICOLON
+        """
+
+    def p_condition(self, p: YaccProduction) -> None:
+        """
+        condition : ID LESSTHAN NUMBER
+                  | ID LESSTHANEQUALS NUMBER
+                  | ID GREATERTHAN NUMBER
+                  | ID GREATERTHANEQUALS NUMBER
+                  | ID EQUALSEQUALS NUMBER
+                  | ID EXCLAMATIONEQUALS NUMBER 
+        """
+
+    def p_iteration(self, p: YaccProduction) -> None:
+        """
+        iteration : ID PLUSPLUS
+                  | ID MINUSMINUS
+        """
+
+    def p_block(self, p: YaccProduction) -> None:
+        """
+        block : OPENBRACE CLOSEBRACE 
+        """
+
+    def p_for_loop(self, p: YaccProduction) -> None:
+        """
+        assignment : FOR OPENPAREN assignment_type ID EQUALS NUMBER SEMICOLON condition SEMICOLON iteration CLOSEPAREN block SEMICOLON
+        """
+
+    def p_parameter(self, p: YaccProduction) -> None:
+        """
+        parameter : ID COLON data_type 
+        """
+    
+    def p_parameters(self, p: YaccProduction) -> None:
+        """
+        parameters : parameter 
+                   | parameter COMMA parameters
+        """
+
+    def p_arrow_function(self, p: YaccProduction) -> None:
+        """
+        assignment : assignment_type ID EQUALS OPENPAREN parameters CLOSEPAREN COLON data_type ARROW block SEMICOLON
+        """
+
     def p_error(self, p: YaccProduction) -> None:
         raise ParserSyntaxError(p)
 
@@ -71,7 +129,7 @@ class Parser:
 # estructuras de datos
 # -----------------------------------------------------------------------------
 
-# 1. array C
+# 1. array #let arreglo: number[] = [1,2,3,4]; C
 # 2. object P
 # 3. ??? tuple J
 
